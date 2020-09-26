846ddfc7872ad425406b4d9adf4d0932e9bfee6e
cricri-pingouin <64480118+cricri-pingouin@users.noreply.github.com>
2020-09-26 02:01:57 +0100

    Add files via upload

--- FmMain.pas
+++ FmMain.pas
@@ -1,13 +1,13 @@
-Unit FmMain;
+unit FmMain;
 
-Interface
+interface
 
-Uses
+uses
   Messages, SysUtils, Classes, Controls, Forms, Grids, XMLDoc, XMLIntf, sevenzip,
-  StdCtrls, ExtCtrls, StrUtils; //, Dialogs;
+  StdCtrls, ExtCtrls, StrUtils;
 
-Type
-  TfrmType = Class(TForm)
+type
+  TfrmType = class(TForm)
     strgrdDropped: TStringGrid;
     lblDropped: TLabel;
     lblSelected: TLabel;
@@ -23,105 +23,105 @@ Type
     lblFileNum: TLabel;
     lblFileSize: TLabel;
     edtFileName: TEdit;
-    Procedure FormCreate(Sender: TObject);
-    Procedure FormDestroy(Sender: TObject);
-    Procedure strgrdDroppedClick(Sender: TObject);
-    Procedure btnFilterClick(Sender: TObject);
-    Procedure strgrdFilteredClick(Sender: TObject);
-  Private
-    Procedure WMDropFiles(Var Msg: TWMDropFiles); Message WM_DROPFILES;
-  Public
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+    procedure strgrdDroppedClick(Sender: TObject);
+    procedure btnFilterClick(Sender: TObject);
+    procedure strgrdFilteredClick(Sender: TObject);
+  private
+    procedure WMDropFiles(var Msg: TWMDropFiles); message WM_DROPFILES;
+  public
     { Public declarations }
 
-  End;
+  end;
 
-Var
+var
   frmType: TfrmType;
-  HeadDB, DescDB: Array Of String; //XML file contents: header, description
-  ClsidDB: Array Of TGUID; //XML file contents: TGUID
+  HeadDB, DescDB: array of string; //XML file contents: header, description
+  ClsidDB: array of TGUID; //XML file contents: TGUID
   DroppedArchivesCount: Integer;
-  DroppedArchivesFullNames: Array Of String;
-  DroppedArchivesCLSID: Array Of TGUID;
+  DroppedArchivesFullNames: array of string;
+  DroppedArchivesCLSID: array of TGUID;
   DBsize: Integer; //Number of nodes in XML file
   FilesDropped: Boolean;       //Flag to trap error if clicking grid before any files dropped
   ReadFileHeaderSize: Integer; //Max length of header to read
 
-Implementation
+implementation
 
-Uses
+uses
   ShellAPI, UFileCatcher;
 
 {$R *.dfm}
 
-Function IntToStrDelimited(aNum: integer): String;
+function IntToStrDelimited(aNum: integer): string;
 // Formats the integer aNum with the default Thousand Separator
-Var
+var
   D: Double;
-Begin
+begin
   D := aNum;
   Result := Format('%.0n', [D]); // ".0" -> no decimals, n -> thousands separators
-End;
+end;
 
-Function GetSizeOfFile(Const FileName: String): Int64;
-Var
+function GetSizeOfFile(const FileName: string): Int64;
+var
   Rec: TSearchRec;
-Begin
+begin
   Result := 0;
-  If (FindFirst(FileName, faAnyFile, Rec) = 0) Then
-  Begin
+  if (FindFirst(FileName, faAnyFile, Rec) = 0) then
+  begin
     Result := Rec.Size;
     FindClose(Rec);
-  End;
-End;
+  end;
+end;
 
-Function ExtractFileNameWoExt(Const FileName: String): String;
-Var
+function ExtractFileNameWoExt(const FileName: string): string;
+var
   i: integer;
-Begin
+begin
   i := LastDelimiter('.' + PathDelim + DriveDelim, FileName);
-  If (i = 0) Or (FileName[i] <> '.') Then
+  if (i = 0) or (FileName[i] <> '.') then
     i := MaxInt;
   Result := ExtractFileName(Copy(FileName, 1, i - 1));
-End;
+end;
 
-Function ReadFileHeader(Const FileName: String): String;
-Var
+function ReadFileHeader(const FileName: string): string;
+var
   Stream: TFileStream;
-  Buffer: Array Of AnsiChar;
+  Buffer: array of AnsiChar;
   i: Integer;
-Begin
+begin
   SetLength(Buffer, ReadFileHeaderSize);
   //Populate buffer elements
   Stream := TFileStream.Create(FileName, fmOpenRead);
-  Try
+  try
     Stream.Read(Buffer[0], ReadFileHeaderSize);
-  Finally
+  finally
     Stream.Free;
-  End;
+  end;
   Result := '';
-  For i := 0 To ReadFileHeaderSize Do
+  for i := 0 to ReadFileHeaderSize do
     Result := Result + IntToHex(Ord(Buffer[i]), 2) + ' ';
-End;
+end;
 
-Function CompareVsHeader(Const FileHeader: String; Const BaseHeader: String): boolean;
-Var
+function CompareVsHeader(const FileHeader: string; const BaseHeader: string): boolean;
+var
   i: Integer;
-Begin
+begin
   Result := True;
-  For i := 0 To Length(BaseHeader) Do
-    If (BaseHeader[i] <> ' ') And (BaseHeader[i] <> '?') And (BaseHeader[i] <> FileHeader[i]) Then
-    Begin
+  for i := 0 to Length(BaseHeader) do
+    if (BaseHeader[i] <> ' ') and (BaseHeader[i] <> '?') and (BaseHeader[i] <> FileHeader[i]) then
+    begin
       Result := False;
       Exit;
-    End;
-End;
+    end;
+end;
 
-Procedure TfrmType.FormCreate(Sender: TObject);
-Var
+procedure TfrmType.FormCreate(Sender: TObject);
+var
   DOC: IXMLDocument;
   FileTypeNode: IXMLNode;
   i, j: Integer;
-Begin
+begin
   //Init StringGrid headers
   //Don't mind doing it here as I needed to copy/paste when clearing grids!
   strgrdDropped.Cells[0, 0] := 'File name';
@@ -144,42 +144,42 @@ Begin
   SetLength(DescDB, DBsize);
   SetLength(ClsidDB, DBsize);
   ReadFileHeaderSize := 0;
-  For i := 0 To DBsize - 1 Do
-  Begin
+  for i := 0 to DBsize - 1 do
+  begin
     FileTypeNode := DOC.ChildNodes.Nodes['filetypes'].ChildNodes[i];
     HeadDB[i] := FileTypeNode.ChildNodes['header'].NodeValue;
     j := length(HeadDB[i]);
-    If (j > ReadFileHeaderSize) Then
+    if (j > ReadFileHeaderSize) then
       ReadFileHeaderSize := j;
     DescDB[i] := FileTypeNode.ChildNodes['description'].NodeValue;
     ClsidDB[i] := StringToGUID(FileTypeNode.ChildNodes['clsid'].NodeValue);
-  End;
-  ReadFileHeaderSize := (ReadFileHeaderSize + 1) Div 3; //Header in the form XX YY ZZ, so 3 characters per char
+  end;
+  ReadFileHeaderSize := (ReadFileHeaderSize + 1) div 3; //Header in the form XX YY ZZ, so 3 characters per char
   // Tell windows we accept file drops
   DragAcceptFiles(Self.Handle, True);
   FilesDropped := False; //Flag to check files qwere dropped before doing stuff
-End;
+end;
 
-Procedure TfrmType.FormDestroy(Sender: TObject);
-Begin
+procedure TfrmType.FormDestroy(Sender: TObject);
+begin
   // Cancel acceptance of file drops
   DragAcceptFiles(Self.Handle, False);
-End;
+end;
 
-Procedure TfrmType.WMDropFiles(Var Msg: TWMDropFiles);
-Var
+procedure TfrmType.WMDropFiles(var Msg: TWMDropFiles);
+var
   i, j, FileCount: Word;
   Catcher: TFileCatcher; //File catcher class
-  FullName, FileHeader, ThisDesc: String;
+  FullName, FileHeader, ThisDesc: string;
   ThisFileSize: Int64;
-Begin
-  Inherited;
+begin
+  inherited;
   // Create file catcher object to hide all messy details
   Catcher := TFileCatcher.Create(Msg.Drop);
   FileCount := Pred(Catcher.FileCount) + 1; //Not sure why +1 needed
   //Clear TStringGrid
   strgrdDropped.Visible := False;
-  For i := 0 To strgrdDropped.ColCount - 1 Do
+  for i := 0 to strgrdDropped.ColCount - 1 do
     strgrdDropped.Cols[i].Clear;
   strgrdDropped.RowCount := 2; //1 for header
   strgrdDropped.Cells[0, 0] := 'File name';
@@ -191,10 +191,11 @@ Begin
   DroppedArchivesCount := 0;
   SetLength(DroppedArchivesFullNames, 0);
   SetLength(DroppedArchivesCLSID, 0);
-  Try
+  try
     //Try to add each dropped file to display
-    For i := 0 To FileCount - 1 Do  //-1 due to base 0
-    Begin
+    for i := 0 to FileCount - 1 do  //-1 due to base 0
+    begin
+      Application.ProcessMessages;
       //Get file properties
       FullName := Catcher.Files[i];
       //FullFilesNames.Add(FullName);
@@ -202,15 +203,15 @@ Begin
       FileHeader := ReadFileHeader(FullName);
       //Identify file header
       ThisDesc := '';
-      For j := 0 To DBsize - 1 Do
-        If CompareVsHeader(FileHeader, HeadDB[j]) Then
-        Begin
+      for j := 0 to DBsize - 1 do
+        if CompareVsHeader(FileHeader, HeadDB[j]) then
+        begin
           ThisDesc := DescDB[j];
           Break;
-        End;
+        end;
       //If header recognised, add to list
-      If (ThisDesc <> '') Then
-      Begin
+      if (ThisDesc <> '') then
+      begin
         //Store info outside grid
         DroppedArchivesCount := DroppedArchivesCount + 1;
         SetLength(DroppedArchivesFullNames, DroppedArchivesCount);
@@ -221,38 +222,38 @@ Begin
         strgrdDropped.RowCount := DroppedArchivesCount + 1;
         strgrdDropped.Cells[0, DroppedArchivesCount] := ExtractFileNameWoExt(FullName);
         strgrdDropped.Cells[1, DroppedArchivesCount] := UpperCase(StringReplace(ExtractFileExt(FullName), '.', '', []));
-        strgrdDropped.Cells[2, DroppedArchivesCount] := IntToStrDelimited(ThisFileSize Div 1024);
+        strgrdDropped.Cells[2, DroppedArchivesCount] := IntToStrDelimited(ThisFileSize div 1024);
         strgrdDropped.Cells[3, DroppedArchivesCount] := FileHeader;
         strgrdDropped.Cells[4, DroppedArchivesCount] := ThisDesc;
 
-      End;
-    End;
-  Finally
+      end;
+    end;
+  finally
     Catcher.Free;
-  End;
+  end;
   //Notify Windows we handled message
   Msg.Result := 0;
-  If DroppedArchivesCount > 0 Then
-  Begin
+  if DroppedArchivesCount > 0 then
+  begin
     FilesDropped := True;
     lblDropped.Caption := 'Recognised compressed files/archives dropped: ' + IntToStr(DroppedArchivesCount);
-  End
-  Else
+  end
+  else
     FilesDropped := False;
   strgrdDropped.Visible := True;
 
-End;
+end;
 
-Procedure TfrmType.strgrdDroppedClick(Sender: TObject);
-Var
+procedure TfrmType.strgrdDroppedClick(Sender: TObject);
+var
   i: Integer;
-Begin
+begin
   //Make sure files were dropped, otherwise program would crash
-  If Not FilesDropped Then
+  if not FilesDropped then
     Exit;
   //Clear TStringGrid
   strgrdSelected.Visible := False;
-  For i := 0 To strgrdSelected.ColCount - 1 Do
+  for i := 0 to strgrdSelected.ColCount - 1 do
     strgrdSelected.Cols[i].Clear;
   strgrdSelected.RowCount := 2; //1 for header
   strgrdSelected.Cells[0, 0] := 'File path';
@@ -260,142 +261,143 @@ Begin
   strgrdSelected.Cells[2, 0] := 'Extension';
   strgrdSelected.Cells[3, 0] := 'Size (KB)';
   //Populate TStringGrid with selected archive contents
-  With CreateInArchive(DroppedArchivesCLSID[strgrdDropped.Row - 1]) Do
-  Begin
+  with CreateInArchive(DroppedArchivesCLSID[strgrdDropped.Row - 1]) do
+  begin
     OpenFile(DroppedArchivesFullNames[strgrdDropped.Row - 1]);
-    For i := 0 To NumberOfItems - 1 Do
-      If Not ItemIsFolder[i] Then
-      Begin
+    for i := 0 to NumberOfItems - 1 do
+      if not ItemIsFolder[i] then
+      begin
         strgrdSelected.Cells[0, strgrdSelected.RowCount - 1] := ExtractFilePath(ItemPath[i]);
         strgrdSelected.Cells[1, strgrdSelected.RowCount - 1] := ExtractFileNameWoExt(ItemPath[i]);
         strgrdSelected.Cells[2, strgrdSelected.RowCount - 1] := UpperCase(StringReplace(ExtractFileExt(ItemPath[i]), '.', '', []));
-        strgrdSelected.Cells[3, strgrdSelected.RowCount - 1] := IntToStr(ItemSize[i] Div 1024);
+        strgrdSelected.Cells[3, strgrdSelected.RowCount - 1] := IntToStr(ItemSize[i] div 1024);
         strgrdSelected.RowCount := strgrdSelected.RowCount + 1;
-      End;
-  End;
+      end;
+  end;
   //Only remove last row if matches were found!
-  If strgrdSelected.RowCount > 2 Then
+  if strgrdSelected.RowCount > 2 then
     strgrdSelected.RowCount := strgrdSelected.RowCount - 1;
   strgrdSelected.Visible := true;
   lblSelected.Caption := 'Files in selected compressed file/archive: ' + IntToStr(strgrdSelected.RowCount - 1);
-End;
+end;
 
-Procedure TfrmType.strgrdFilteredClick(Sender: TObject);
-Var
+procedure TfrmType.strgrdFilteredClick(Sender: TObject);
+var
   i, SelectedRow: Integer;
-Begin
+begin
   SelectedRow := strgrdFiltered.Row;
-  For i := 1 To strgrdDropped.RowCount - 1 Do
-    If ((strgrdFiltered.Cells[0, SelectedRow] = strgrdDropped.Cells[0, i]) And (strgrdFiltered.Cells[1, SelectedRow] = strgrdDropped.Cells[1, i]) And (strgrdFiltered.Cells[2, SelectedRow] = strgrdDropped.Cells[2, i]) And (strgrdFiltered.Cells[3, SelectedRow] = strgrdDropped.Cells[4, i])) Then
-    Begin
+  for i := 1 to strgrdDropped.RowCount - 1 do
+    if ((strgrdFiltered.Cells[0, SelectedRow] = strgrdDropped.Cells[0, i]) and (strgrdFiltered.Cells[1, SelectedRow] = strgrdDropped.Cells[1, i]) and (strgrdFiltered.Cells[2, SelectedRow] = strgrdDropped.Cells[2, i]) and (strgrdFiltered.Cells[3, SelectedRow] = strgrdDropped.Cells[4, i])) then
+    begin
       strgrdDropped.Row := i;
       Break;
-    End;
-End;
+    end;
+end;
 
-Procedure TfrmType.btnFilterClick(Sender: TObject);
-Var
+procedure TfrmType.btnFilterClick(Sender: TObject);
+var
   i, j: Integer;
   FileNum, FileNumLimit: LongInt;
   Filter, StringFound: Boolean;
   MaxFileSize, MinFileSize, ThisSize, FileSizeLimit: Int64;
-  MatchString: String;
-Begin
+  MatchString: string;
+begin
   //Make sure files were dropped, otherwise program would crash
-  If Not FilesDropped Then
+  if not FilesDropped then
     Exit;
   //Clear TStringGrid
   strgrdFiltered.Visible := False;
-  For i := 0 To strgrdFiltered.ColCount - 1 Do
+  for i := 0 to strgrdFiltered.ColCount - 1 do
     strgrdFiltered.Cols[i].Clear;
   strgrdFiltered.RowCount := 2; //1 for header
   strgrdFiltered.Cells[0, 0] := 'File name';
   strgrdFiltered.Cells[1, 0] := 'Extension';
   strgrdFiltered.Cells[2, 0] := 'Size (KB)';
   strgrdFiltered.Cells[3, 0] := 'Type detected';
-  For i := 0 To DroppedArchivesCount - 1 Do
-  Begin
-  //Populate TStringGrid with selected archive contents
-    With CreateInArchive(DroppedArchivesCLSID[i]) Do
-    Begin
+  for i := 0 to DroppedArchivesCount - 1 do
+  begin
+    Application.ProcessMessages;
+    //Populate TStringGrid with selected archive contents
+    with CreateInArchive(DroppedArchivesCLSID[i]) do
+    begin
       OpenFile(DroppedArchivesFullNames[i]);
       Filter := True;
       //Filter file number
       //rgFileNum.ItemIndex: 0: at least, 1: at most, 2: exactly
       FileNumLimit := StrToInt64(edtFileNum.Text);
-      If (FileNumLimit > 0) Then
-      Begin
+      if (FileNumLimit > 0) then
+      begin
       //Count files only, i.e. exclude folders
         FileNum := 0;
-        For j := 0 To NumberOfItems - 1 Do
-          If Not ItemIsFolder[j] Then
+        for j := 0 to NumberOfItems - 1 do
+          if not ItemIsFolder[j] then
             FileNum := FileNum + 1;
-        If ((rgFileNum.ItemIndex = 0) And (FileNum < FileNumLimit)) Or ((rgFileNum.ItemIndex = 1) And (FileNum > FileNumLimit)) Or ((rgFileNum.ItemIndex = 2) And (FileNum <> FileNumLimit)) Then
+        if ((rgFileNum.ItemIndex = 0) and (FileNum < FileNumLimit)) or ((rgFileNum.ItemIndex = 1) and (FileNum > FileNumLimit)) or ((rgFileNum.ItemIndex = 2) and (FileNum <> FileNumLimit)) then
           Filter := False;
-      End;
+      end;
       //File number matches filter: test file size
       //rgFileSize.ItemIndex: 0: at least 1 file smaller, 1: at least 1 file bigger, 2: no file smaller, 3: no file bigger
       FileSizeLimit := StrToInt64(edtFileSize.Text);
-      If (Filter And (FileSizeLimit > 0)) Then
-      Begin
+      if (Filter and (FileSizeLimit > 0)) then
+      begin
         //Get min and max files sizes
         MaxFileSize := 0;
         MinFileSize := MaxLongInt;
-        For j := 0 To NumberOfItems - 1 Do
-          If Not ItemIsFolder[j] Then
-          Begin
-            ThisSize := ItemSize[j] Div 1024;
-            If (ThisSize > MaxFileSize) Then
+        for j := 0 to NumberOfItems - 1 do
+          if not ItemIsFolder[j] then
+          begin
+            ThisSize := ItemSize[j] div 1024;
+            if (ThisSize > MaxFileSize) then
               MaxFileSize := ThisSize;
-            If (ThisSize < MinFileSize) Then
+            if (ThisSize < MinFileSize) then
               MinFileSize := ThisSize;
-          End;
-        If ((rgFileSize.ItemIndex = 0) And (MinFileSize > FileSizeLimit)) Or ((rgFileSize.ItemIndex = 1) And (MaxFileSize < FileSizeLimit)) Or ((rgFileSize.ItemIndex = 2) And (MinFileSize < FileSizeLimit)) Or ((rgFileSize.ItemIndex = 3) And (MaxFileSize > FileSizeLimit)) Then
+          end;
+        if ((rgFileSize.ItemIndex = 0) and (MinFileSize > FileSizeLimit)) or ((rgFileSize.ItemIndex = 1) and (MaxFileSize < FileSizeLimit)) or ((rgFileSize.ItemIndex = 2) and (MinFileSize < FileSizeLimit)) or ((rgFileSize.ItemIndex = 3) and (MaxFileSize > FileSizeLimit)) then
           Filter := False;
-      End;
+      end;
       //File size matches filter: test file names
       //rgFileName: 0: at least one file containing, 1: no file containing
       MatchString := edtFileName.Text;
-      If (Filter And (MatchString <> '')) Then //if empty string: skip
-      Begin
-        For j := 0 To NumberOfItems - 1 Do
-          If Not ItemIsFolder[j] Then
-          Begin
+      if (Filter and (MatchString <> '')) then //if empty string: skip
+      begin
+        for j := 0 to NumberOfItems - 1 do
+          if not ItemIsFolder[j] then
+          begin
             StringFound := ContainsText(ExtractFileName(ItemPath[j]), MatchString);
             //String found and we want it
-            If ((rgFileName.ItemIndex = 0) And StringFound) Then
+            if ((rgFileName.ItemIndex = 0) and StringFound) then
               Break;
             //String found and we don't want it
-            If ((rgFileName.ItemIndex = 1) And StringFound) Then
-            Begin
+            if ((rgFileName.ItemIndex = 1) and StringFound) then
+            begin
               Filter := False;
               Break
-            End;
-          End;
+            end;
+          end;
         //String not found (in last file since we exited for loop) and we want it
-        If ((rgFileName.ItemIndex = 0) And Not StringFound) Then
+        if ((rgFileName.ItemIndex = 0) and not StringFound) then
           Filter := False;
-      End;
+      end;
       //Filter still true: add to list
-      If Filter Then
-      Begin
+      if Filter then
+      begin
         strgrdFiltered.Cells[0, strgrdFiltered.RowCount - 1] := strgrdDropped.Cells[0, i + 1];
         strgrdFiltered.Cells[1, strgrdFiltered.RowCount - 1] := strgrdDropped.Cells[1, i + 1];
         strgrdFiltered.Cells[2, strgrdFiltered.RowCount - 1] := strgrdDropped.Cells[2, i + 1];
         strgrdFiltered.Cells[3, strgrdFiltered.RowCount - 1] := strgrdDropped.Cells[4, i + 1];
         strgrdFiltered.RowCount := strgrdFiltered.RowCount + 1;
-      End;
-    End;
-  End;
+      end;
+    end;
+  end;
   //Only remove last row if matches were found!
-  If strgrdFiltered.RowCount > 2 Then
+  if strgrdFiltered.RowCount > 2 then
     strgrdFiltered.RowCount := strgrdFiltered.RowCount - 1;
   strgrdFiltered.Visible := true;
-  If strgrdFiltered.Cells[0, 1] <> '' Then
+  if strgrdFiltered.Cells[0, 1] <> '' then
     lblFiltered.Caption := 'Matching archives: ' + IntToStr(strgrdFiltered.RowCount - 1)
-  Else
+  else
     lblFiltered.Caption := 'Matching archives: 0';
-End;
+end;
 
-End.
+end.
 
